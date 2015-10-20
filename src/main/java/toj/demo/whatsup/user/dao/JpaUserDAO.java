d2efd32d6246ec1ef4d152843e8793970f8285c2
Mihai Popovici <mihai.popovici@softvision.ro>
2015-10-20 16:30:26 +0300

    minor changes to UserDAO

--- src/main/java/toj/demo/whatsup/user/dao/JpaUserDAO.java
+++ src/main/java/toj/demo/whatsup/user/dao/JpaUserDAO.java
@@ -4,11 +4,9 @@ import org.springframework.stereotype.Repository;
 import toj.demo.whatsup.dao.JpaDAO;
 import toj.demo.whatsup.domain.User;
 import toj.demo.whatsup.domain.User_;
-
 import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaDelete;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.util.Optional;
@@ -23,7 +21,7 @@ public class JpaUserDAO extends JpaDAO<User, Long> implements UserDAO {
     public boolean checkUser(String name, String password) {
         try {
             CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-            CriteriaQuery cq = cb.createQuery(this.entityClass);
+            CriteriaQuery<User> cq = cb.createQuery(this.entityClass);
             Root<User> userRoot = cq.from(this.entityClass);
             cq.where(
                     cb.and(
